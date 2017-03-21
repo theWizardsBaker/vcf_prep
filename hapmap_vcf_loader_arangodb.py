b259fd22dda8ecb29a45b7bfbc1a1f91a2643769
Justin Le Tourneau <justin.letourn@gmail.com>
2017-03-21 13:25:16 -0500

    fixed the regular expression splitting

--- hapmap_vcf_loader_arangodb.py
+++ hapmap_vcf_loader_arangodb.py
@@ -29,7 +29,7 @@ class hapmap_load:
 					if self.logging:
 						print "Loading header" 
 					# remove the starting ## and split on the first =
-					header_line = line[2:].split(r'=', 1)
+					header_line = line[2:].split('=', 1)
 					# write to the appropriate hash
 					if header_line[0] == "INFO":
 						self.load_header(header_line[1], info_structure)
@@ -46,7 +46,7 @@ class hapmap_load:
 					else:
 						db_samples = database.collection('samples')
 					# split the line on the tab and drop the first 9 elements (they are not samples)
-					self.table_columns = (line.split(r'\t+'))[9:]
+					self.table_columns = (re.split(r'\t+', line))[9:]
 					# walk over each element
 					for sample in self.table_columns:
 						# and add it into our database
@@ -55,7 +55,7 @@ class hapmap_load:
 					if self.logging:
 						print "Loading Variant"
 					# split the line on the tabs
-					variant_calls = line.split(r'\t+')
+					variant_calls = re.split(r'\t+', line)
 					# look for the 'variant' collection 
 					if not len(filter(lambda x: x['name']=='variants', database.collections())):
 						# if we don't find it, let's create it
@@ -65,25 +65,25 @@ class hapmap_load:
 					# create the variant object
 					variant = { 
 								'_key': variant_calls[2],
-								'names': variant_calls[2].split(r','),
+								'names': variant_calls[2].split(','),
 								'chromosome': int(variant_calls[0]),
 								'position': variant_calls[1],
 								'filter': variant_calls[6],
 								'reference_base': variant_calls[3],
-								'alternate_bases': variant_calls[4].strip(r'<|>').split(r','),
+								'alternate_bases': variant_calls[4].strip(r'<|>').split(','),
 								'alternate_structure': alt_structure,
 								'info': {}
 							  }
 					# walk over each info value
-					for info_line in variant_calls[7].split(r';'):
+					for info_line in variant_calls[7].split(';'):
 						# cut apart at the ='s
-						info_line = info_line.split(r'=')
+						info_line = info_line.split('=')
 						# copy the variant info 
 						variant['info'][info_line[0]] = copy.deepcopy(info_structure[info_line[0]])
 						# find out how to store each value for the infos
 						values = []
 						# get each value
-						for elm in info_line[1].split(r','):
+						for elm in info_line[1].split(','):
 							raw_type = variant['info'][info_line[0]]["type"].lower()
 							# determin the type of the value
 							if raw_type == 'integer':
@@ -107,7 +107,7 @@ class hapmap_load:
 							variant_call = { 
 								'variant': line[2], 
 								'phased': re.search(r'\|', call) != None , 
-								'genotype': map(lambda x: int(x), call.split(r'\||\/'))
+								'genotype': map(lambda x: int(x), re.split(r'\||\/', call))
 							}
 							# get the sample
 							samp = samples.get(self.table_columns[ind])
@@ -127,10 +127,10 @@ class hapmap_load:
 		# remove the < >
 		header_line = re.sub(r'<|>', '', header_line)
 		# format the header line so that we have an array of list elements
-		header_line = re.compile(',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)').split(header_line)
+		header_line = re.split(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)', header_line)
 		# reference element in the info structure
 		elm_ref = ''
-		if len(header_line) > 1
+		if len(header_line) > 1:
 			# each header line
 			for line in header_line:
 				val = line.split(r'=')
